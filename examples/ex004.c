b1d94a8ab79f66a2777f9d99be68ee5f13ae4d23
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2022-12-12 23:33:54 +0100

    exemple rester plus parlant

--- examples/ex004.c
+++ examples/ex004.c
@@ -26,13 +26,12 @@ void incr_init (struct incr_mem* mem) {
   mem->aux__4_next1 = 0;
 }
 
-int incr (struct incr_mem* mem, int tic) {
+int incr (struct incr_mem* mem) {
   int aux__4;
   int aux__3;
   int aux__2;
   int aux__1;
   int cpt;
-  enum inductive_bool switch_1;
   
   aux__1 = 0;
   
@@ -40,18 +39,7 @@ int incr (struct incr_mem* mem, int tic) {
   
   aux__4 = mem->aux__4_next1;
   
-  switch (tic) {
-    case TRUE: {
-      switch_1 = 1;
-      break;
-    }
-    case FALSE: {
-      switch_1 = 0;
-      break;
-    }
-  };
-  
-  cpt = (aux__2 ? 0 : aux__4 + switch_1);
+  cpt = (aux__2 ? 0 : aux__4 + 1);
   
   aux__3 = cpt;
   
@@ -64,56 +52,28 @@ int incr (struct incr_mem* mem, int tic) {
 
 struct check_mem {
   struct incr_mem incr_next1;
-  int aux__7_next4;
-  int aux__9_next3;
 };
 
 void check_init (struct check_mem* mem) {
-  mem->aux__7_next4 = 1;
-  
-  mem->aux__9_next3 = 0;
-  
   incr_init(&(mem->incr_next1));
 }
 
 int check (struct check_mem* mem, int x) {
-  int aux__9;
-  int aux__8;
-  int aux__7;
-  int aux__6;
   int aux__5;
-  int cpt;
-  int ok;
+  int o;
   int call_1;
   
-  switch (x) {
-    case 1: {
-      incr_init(&(mem->incr_next1));
-      break;
-    }
+  if (x) {
+    incr_init(&(mem->incr_next1));
   };
   
-  call_1 = incr(&(mem->incr_next1), x);
+  call_1 = incr(&(mem->incr_next1));
   
   aux__5 = call_1;
   
-  aux__6 = 0;
+  o = aux__5;
   
-  aux__7 = mem->aux__7_next4;
-  
-  aux__9 = mem->aux__9_next3;
-  
-  cpt = aux__5;
-  
-  ok = (aux__7 ? 0 : aux__9 <= cpt);
-  
-  aux__8 = cpt;
-  
-  mem->aux__7_next4 = aux__6;
-  
-  mem->aux__9_next3 = aux__8;
-  
-  return ok;
+  return o;
 }
 
 int main (int argc, char* argv[]) {
